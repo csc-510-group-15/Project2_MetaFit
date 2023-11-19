54390040576c100fdc816840af1801b1d4061907
achetwa <anujchetwani@gmail.com>
2023-11-19 02:09:46 -0500

    fix: remove unique char

--- forms.py
+++ forms.py
@@ -82,7 +82,7 @@ class CalorieForm(FlaskForm):
     for record in cursor:
         get_docs.append(record)
 
-    result = [ß]
+    result = []
     temp = ""
     for i in get_docs:
         temp = i['food'] + ' (' + i['calories'] + ')'
