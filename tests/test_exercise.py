da5315b16bd3a324583a7ff5a6dcb5d5fbbc0ba5
hchen54 <hchen54@ncsu.edu>
2025-02-26 20:06:37 -0500

    [hchen54] update exercise test case to match new code

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,6 @@
 import unittest
 from application import app
 
-
 class ExerciseFinderTests(unittest.TestCase):
 
     @classmethod
@@ -10,41 +9,39 @@ class ExerciseFinderTests(unittest.TestCase):
 
     def test_valid_inputs(self):
         response = self.client.post(
-            '/', data={'muscle': 'biceps', 'difficulty': 'beginner'})
+            '/exercise', data={'muscle': 'biceps', 'difficulty': 'beginner'})
         self.assertIn('Bicep Curl', response.data.decode())
         self.assertIn('Strength', response.data.decode())
 
     def test_empty_muscle_input(self):
         response = self.client.post(
-            '/', data={'muscle': '', 'difficulty': 'beginner'})
+            '/exercise', data={'muscle': '', 'difficulty': 'beginner'})
         self.assertIn('Both muscle and difficulty are required!',
                       response.data.decode())
 
     def test_empty_difficulty_input(self):
         response = self.client.post(
-            '/', data={'muscle': 'biceps', 'difficulty': ''})
+            '/exercise', data={'muscle': 'biceps', 'difficulty': ''})
         self.assertIn('Both muscle and difficulty are required!',
                       response.data.decode())
 
     def test_error_message_display(self):
         response = self.client.post(
-            '/', data={'muscle': 'xyzmuscle', 'difficulty': 'intermediate'})
-        self.assertIn(
-            'No exercises found for the specified muscle and difficulty.', response.data.decode())
+            '/exercise', data={'muscle': 'xyzmuscle', 'difficulty': 'intermediate'})
+        self.assertIn('No exercises found for the specified muscle and difficulty.', response.data.decode())
 
     def test_exercises_rendered(self):
         response = self.client.post(
-            '/', data={'muscle': 'biceps', 'difficulty': 'beginner'})
+            '/exercise', data={'muscle': 'biceps', 'difficulty': 'beginner'})
         self.assertIn('Bicep Curl', response.data.decode())
         self.assertIn('Beginner', response.data.decode())
         self.assertIn('Dumbbell', response.data.decode())
 
     def test_multiple_exercises_rendered(self):
         response = self.client.post(
-            '/', data={'muscle': 'triceps', 'difficulty': 'intermediate'})
+            '/exercise', data={'muscle': 'triceps', 'difficulty': 'intermediate'})
         self.assertIn('Tricep Dips', response.data.decode())
         self.assertIn('Intermediate', response.data.decode())
-
 
 if __name__ == '__main__':
     unittest.main()
